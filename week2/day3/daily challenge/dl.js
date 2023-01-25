af52ff31f975f8f877c9cb858e6412cbc5e4da29
tushilkumar <t.gunness35@gmail.com>
2023-01-25 13:44:59 +0400

    Stars bruh

---
+++ week2/day3/daily challenge/dl.js
@@ -0,0 +1,9 @@
+let patternStar=6
+let star=""
+for (let i=0;i < patternStar; i++){console.log(star=star+"*")}
+//nested loop
+for (let j=0;j< patternStar;j++){
+    let star2="";
+    y=j+1;
+for(let t=0;t<y;t++){star2=star2+"*"};
+console.log(star2)}
