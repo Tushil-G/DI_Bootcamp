58cf3142c3a5e7843d34da4a96c4a8b3b37d555f
github username <t.gunness35@gmail.com>
2023-02-06 12:09:10 +0400

    good

---
+++ python/xp_gold.py
@@ -0,0 +1,37 @@
+#excercise 1
+print("hello world")
+print("hello world")
+print("hello world")
+print("i love python")
+print("i love python")
+print("i love python")
+#excercise 2
+month=int(input("enter month in num: "))
+if month in (range(0,12)):
+    print("~~correct month has been choosen~~")
+if month==1:
+    print("jan")
+if month==2:
+    print("feb")
+if month==3:
+    print("march")    
+if month==4:
+    print("april")
+if month==5:
+    print("may")
+if month==6:
+    print("june")
+if month==7:
+    print("july")
+if month==8:
+    print("august")
+if month==9:
+    print("sept")
+if month==10:
+    print("Oct")
+if month==11:
+    print("nov")
+if month==12:
+    print("dec")    
+else:
+    print("nope")
