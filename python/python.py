7a56124739db8311c98e1ecd80bdae7f14c70833
github username <t.gunness35@gmail.com>
2023-02-06 11:26:26 +0400

    change

---
+++ python/python.py
@@ -0,0 +1,47 @@
+#excercise_1
+print("Hello word")
+print("Hello word")
+print("Hello word")
+print("Hello word")
+#ecercise_2
+x=99
+print((x^3)*8)
+#excercise 3
+5 < 3 # false/0
+3 == 3 # true/0
+3 == "3" # false/0
+# "3" > 3# false/0
+#"Hello" == "hello"#false/0
+#excercise 4
+computer_brand="I have a <computer_brand> computer"
+print(computer_brand)
+#excercise 5
+age="20"
+name="Tushil"
+shoe_size="idk"
+info=print("my name is:", name ," my age is:",age , "my shoe size is:",shoe_size)
+#excercise 6
+a="hello"
+b="world"
+print(f'{a}' +' '+b)#Simpler version print(a, b)
+#excercise 7
+x=int(input("enter a number: "))
+if x%2==0:
+    print("even")
+else:
+    print("odd")
+#excercise 8
+nom=str(input("Enter your name: "))
+if nom=="Tushil":
+    print("bruh")
+else:
+    print("you good bro!")
+#excercise 9
+height=int(input("whats your height: "))
+if height>=145:
+    print("You are tall enough")
+else:
+    print("you Small lol")    
+
+
+
---
+++ week2/day4/xp.js
@@ -0,0 +1,56 @@
+//excercise 1
+// infoAboutMe()
+// function infoAboutMe(){console.log("my name is Tushil")}
+// infoAboutPerson("David", 45, "blue")
+// infoAboutPerson("Josh", 12, "yellow")
+// function infoAboutPerson(personName, personAge, personFavoriteColor){console.log("your name is "+personName)
+// console.log("Your age is "+personAge)
+// console.log("your favorite colour is "+personFavoriteColor)}
+// calculateTip()
+// function calculateTip(){ let amount=prompt("The amount of the bill")
+// let tip
+// if (amount<50){tip=0.2}
+// if (amount<200 && amount >=50){tip=0.15}
+// if (amount>200){tip=0.1}
+// let total=amount*(tip+1);
+// parseFloat(total)
+// console.log("the bill is "+amount)
+// console.log("your bill plus tip is "+total)}
+// isDivisible()
+// function isDivisible(){for(let i=0;i<500;i++){
+//     if (i%23==0){console.log(i)}
+
+// }}
+// const stock = { 
+//     "banana": 6, 
+//     "apple": 0,
+//     "pear": 12,
+//     "orange": 32,
+//     "blueberry":1
+// }  
+
+// const prices = {    
+//     "banana": 4, 
+//     "apple": 2, 
+//     "pear": 1,
+//     "orange": 1.5,
+//     "blueberry":10
+// } 
+// let shoplist=["banana","apple","orange"]
+// mybill()
+// function mybill(){for (const stocks of shoplist){
+//     let amountOfStock=stock[stocks]
+//     if (amountOfStock>0){
+//         const price=prices[stocks]
+//         console.log("the price of "+stocks+" is "+price+"$")
+//     }
+//     else{console.log(stocks+" is not available")}
+
+// }}
+changeEnough(itemPrice, amountOfChange)
+
+
+
+
+
+
