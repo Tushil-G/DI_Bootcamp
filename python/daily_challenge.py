ee92a476fb624e75e6f8c04a485640ee76a64da1
github username <t.gunness35@gmail.com>
2023-02-07 09:42:14 +0400

    ans

--- python/daily_challenge.py
+++ python/daily_challenge.py
@@ -5,6 +5,10 @@ if len(msg)>10:
     print("too long")
 for i in range(len(msg)):
     print(msg[i])
+    x=" "
+for y in msg:
+    x+=y
+    print(x)
     import random
 shuffle="".join(random.sample(msg,len(msg)))
 print(shuffle)
