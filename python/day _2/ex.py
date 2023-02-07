ce1d5259f6c6a2021b4535e9dfc9de4ecbec1fc2
github username <t.gunness35@gmail.com>
2023-02-07 12:04:32 +0400

    feat:ex 1

---
+++ python/day _2/ex.py
@@ -0,0 +1,9 @@
+list1=[1,2,3,4,5,20,20,4]
+replace=list1.index(20)
+list1[replace]=200
+#Replace first one
+print(list1)
+#replace all
+str(list1)
+replace=[200 if item ==20 else item for  item in list1]
+print(replace)
