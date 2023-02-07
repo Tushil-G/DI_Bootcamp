d60c6e3636d79a8767c2f0650731b5d25286063b
github username <t.gunness35@gmail.com>
2023-02-08 00:16:44 +0400

    commit: daily challenge

---
+++ python/day _2/daily.py
@@ -0,0 +1,18 @@
+num=int(input("enter number: "))
+len=int(input("enter length: "))
+for i in range(len):
+   i+=1
+   print(num*i)
+# part 2
+string=input("Enter string: ")
+p=""
+for char in string:
+    if char not in p:
+        p=p+char
+print(p)#removes all duplicates
+y=''
+for x in string:
+    if y=='' or x != y [ len(y)-1]:
+     y=y+x
+print(y)#removes all duplicates
+
---
+++ python/day _2/xp.py
@@ -0,0 +1,65 @@
+# #exercise 1
+# my_fav_numbers={18,19,20,21,22}
+# my_fav_numbers.add(23)
+# my_fav_numbers.add(24)
+# #excercise 2
+# my_fav_numbers=set(list(my_fav_numbers)[:-1])#my_fav_numbers.remove(24)
+# fr_numbers={25,36,77,90}
+# our=fr_numbers.union(my_fav_numbers)
+# print(our)
+# #if combine last digit must be remove our=set(list(our)[:-1])
+
+# #excercise 2
+# #Given a tuple which value is integers, is it possible to add more integers to the tuple?no
+# #excercise 3
+# basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+# basket.remove("Banana")
+# basket.remove("Blueberries")
+# basket.append("kiwi")
+# basket.insert(0,"Apples")
+# print(f' the amount of apples are: {basket.count("Apples")}')
+# basket.clear()
+# print(basket)
+# #excercise 4
+# #What is a float? What is the difference between an integer and a float?float have decimal value for instance 1.3
+# # #Can you think of another way to generate a sequence of floats?yes
+# my_list=[1]
+# num=0
+# while num<8:
+#     my_list.append(my_list[-1]+0.5)
+#     num+=1
+# print(my_list)
+#excecise 5
+# for x in range(1,20):
+#     print(x+1)
+# for x in range(1,21):
+#     if (x % 2) == 0:
+#         print(f'these numbers are even {x}')
+
+ #excercise 6
+# nom=""
+# while nom !="tushil":
+#     nom=input("enter name plz: ")
+# else:
+#     print("you finally remebered your name")
+# excercise 7
+
+# fav_fruit=input("what is your favorite fruit {plz add a space between each fruit}: ")
+# fruit=fav_fruit.split()
+# print(fruit)
+# fruit=input("fruit choice: ")
+# if fruit in fav_fruit:
+#     print("You chose one of your favorite fruits! Enjoy!")
+# else:
+#     print("You chose a new fruit. I hope you enjoy")
+
+# excercise 8
+
+
+
+
+
+
+
+
+
