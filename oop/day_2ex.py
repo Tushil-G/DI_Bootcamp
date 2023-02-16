735518b92a24a6294e98d24a07a1a94d9517b7ab
github username <t.gunness35@gmail.com>
2023-02-16 11:48:33 +0400

    feat: virtual machine/learning about faker/how to access file from terminal

--- oop/day_2ex.py
+++ oop/day_2ex.py
@@ -15,6 +15,17 @@
 # c = Computer()
 # print(c.name)
 # c.sell()
-list_1 = [ 'asdada', 1, 123131.131, 'blaa adaraerada', 0.000001, 34.12451235265, 'stackoverflow is awesome' ]
-list_2 = [num for num in list_1 if isinstance(num, (str))]#print only only string()
-print(list_2)
+# list_1 = [ 'asdada', 1, 123131.131, 'blaa adaraerada', 0.000001, 34.12451235265, 'stackoverflow is awesome' ]
+# list_2 = [num for num in list_1 if isinstance(num, (str))]#print only only string()
+# print(list_2)
+from faker import Faker
+fake = Faker()
+
+# Writing fake names to the file
+with open('output.txt', 'w') as f:
+    for i in range(10):
+       f.write(f'{fake.name()}\n')
+
+# Reading from the file jsut created
+for line in open('output.txt'):
+    print(line)
