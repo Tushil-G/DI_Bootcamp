83f07446e0b749cc2eb2e21d613783da172c862a
github username <t.gunness35@gmail.com>
2023-02-17 13:56:37 +0400

    add:Time to print for aesthetic

--- oop/day5.py
+++ oop/day5.py
@@ -2,4 +2,4 @@ import requests
 response = requests.get('https://www.google.com/search?client=opera-gx&q=https%3A%2F%2Fgoogle.com&sourceid=opera&ie=UTF-8&oe=UTF-8') 
   
 print(response) 
-print(response.elapsed)
+print(f'Time: {response.elapsed}')
