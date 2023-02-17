9340bfdebd795813fc00f90ad763256fac6602d9
github username <t.gunness35@gmail.com>
2023-02-17 13:52:12 +0400

    feat:time lapse daily challenge day 5

---
+++ oop/day5.py
@@ -0,0 +1,5 @@
+import requests  
+response = requests.get('https://www.google.com/search?client=opera-gx&q=https%3A%2F%2Fgoogle.com&sourceid=opera&ie=UTF-8&oe=UTF-8') 
+  
+print(response) 
+print(response.elapsed)
