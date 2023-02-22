1dc4a09c5a3e0458c28fc34d59c5fa318d553f8e
github username <t.gunness35@gmail.com>
2023-02-22 11:44:26 +0400

    feat:SQL__Daily -challenge

---
+++ sql/sql.sql
@@ -0,0 +1,24 @@
+-- CREATE TABLE actors(
+--  actor_id SERIAL PRIMARY KEY,
+--  first_name VARCHAR (50) NOT NULL,
+--  last_name VARCHAR (100) NOT NULL,
+--  age DATE NOT NULL,
+--  number_oscars SMALLINT NOT NULL
+--)
+-- SELECT * FROM actors;
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Matt','Damon','08/10/1970', 5);
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('George','Clooney','06/05/1961', 2);
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('bob','Cloone','06/05/1960', 5);
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('mars','Cooney','02/12/1900', 90);
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('George','Clooney','06/05/1961', 2),('wan','james','03/05/2022',8),('ben','fame','04/12/2000',9);
+
+SELECT COUNT (*) FROM actors;
+
+
+//ex2//--The Count will increase
+
