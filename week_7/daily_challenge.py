a1abc7f96c3d9141ae49add6c9224b1f57e69255
github username <t.gunness35@gmail.com>
2023-02-27 16:17:20 +0400

    feat:daily

---
+++ week_7/daily_challenge.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+M=np.random.randint(2, 49)
+N=np.random.randint(2, 39)
+
+# print(M)
+# print(N)
+
+table = np.random.randint(1, 100, size=(M,N))
+M=M+7#not sure
+N=N+7#not sure
+print(table)
+
+
+
+
+
+
+
