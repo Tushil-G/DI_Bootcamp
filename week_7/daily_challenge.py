3148ce185261495965e755f733e81c60c620b8d2
github username <t.gunness35@gmail.com>
2023-02-28 10:21:06 +0400

    feat:daily challenge complete

--- week_7/daily_challenge.py
+++ week_7/daily_challenge.py
@@ -2,14 +2,16 @@ import numpy as np
 
 M=np.random.randint(2, 49)
 N=np.random.randint(2, 39)
-
+if M<50 and N<40:
 # print(M)
 # print(N)
-
-table = np.random.randint(1, 100, size=(M,N))
-M=M+7#not sure
-N=N+7#not sure
-print(table)
+    table = np.random.randint(1, 100, size=(M,N))
+    # print(table[:2])
+    # print(table[:,2])
+    # table[-1] = 7
+    # print(table)
+    table[:,-1] = table[:,0] + table[:,1]
+    print(table)
 
 
 
